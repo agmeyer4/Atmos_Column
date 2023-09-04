7964037cb86c8ba8ab464cdb5fb652b1c14cf6a0
Aaron Meyer <agmeyer4@gmail.com>
2023-09-04 14:35:41 -0600

    added test results for oof

---
+++ atmos_column/funcs/test_results_for_oof.py
@@ -0,0 +1,19 @@
+import os
+results_folder = input('Base Results folder with cumulative/daily and date folders : ')
+if not os.path.isdir(results_folder):
+    raise NameError('No path of that name')
+daily_folder = os.path.join(results_folder,'daily')
+dates_list = os.listdir(daily_folder)
+for date in sorted(dates_list):
+    files = os.listdir(os.path.join(daily_folder,date))
+    oof_exists = False
+    for file in files:
+        if file.endswith('.vav.ada.aia.oof'):
+            oof_exists = True
+            break
+        else: 
+            continue
+    if oof_exists:
+        print(f'OOF exists for {date}')
+    else:
+        print(f'***NO OOF FILE FOR {date}***')
