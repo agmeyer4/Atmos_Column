f0389b4c7a225bf65abab48f96e78fc78c188d38
Aaron Meyer <agmeyer4@gmail.com>
2023-09-14 20:37:49 -0600

    plotting updates

--- atmos_column/funcs/openair_em27_v1.r
+++ atmos_column/funcs/openair_em27_v1.r
@@ -3,9 +3,17 @@ library(grid)
 library(openair)
 library(latticeExtra)
 
-df<-read.csv('~/LAIR/Data/flag0_interesting.csv')
-df1<-head(df,10000)
-#windRose(df)
+df<-read.csv('/uufs/chpc.utah.edu/common/home/u0890904/LAIR_1/Data/csv_for_r/merged_dbk_20230805.csv')
 
-polarPlot(df1,pollutant = 'xch4.ppm.',
-                     statistic = 'cpf',percentile = 95,cols = 'viridis')
+polarPlot(df,pollutant = 'corrected_xch4.ppm.',
+                     statistic = 'percentile',percentile = 95,cols = 'viridis',par.settings=list(fontsize=list(text=25)))
+
+
+df<-read.csv('/uufs/chpc.utah.edu/common/home/u0890904/LAIR_1/Data/csv_for_r/merged_dbk_20230804.csv')
+
+polarPlot(df,pollutant = 'corrected_xch4.ppm.',
+          statistic = 'percentile',percentile = 95,cols = 'viridis',par.settings=list(fontsize=list(text=25)))
+
+
+
+
