6d54d763a4361d177a21bcf5e724c2b9944c7c11
Aaron Meyer <agmeyer4@gmail.com>
2023-11-28 14:13:39 -0700

    documented mesowest

--- atmos_column/funcs/mesowest_met_handler.py
+++ atmos_column/funcs/mesowest_met_handler.py
@@ -1,3 +1,33 @@
+'''
+Script: mesowest_met_handler.py
+Author: Aaron G. Meyer
+Email: agmeyer4@gmail.com
+
+This module is for formatting mesowest met data into a format that is ingestable by GGG by splitting a monthly mesowest
+csv file into daily met files of the correct form and format. It does not include any "data grabbing" from the mesowest 
+site -- this must be done manually for now. To use this module, follow the following steps:
+
+1. Login to mymesowest (https://mesowest.utah.edu/cgi-bin/droman/my_mesowest.cgi)
+2. Go to the main site (https://mesowest.utah.edu/). At left, under "station search", select site id from the 
+   dropdown. In the seach box, enter the site id you want (for U of U, search 'wbb'). 
+3. Once on the station page, click "Download Data" option on the left 
+4. Select "Metric" under "Units in" dropdown. 
+5. Select "GMT" under download by date dropdown instead of local (this will propogate to the mothly downloads 
+   in step 6. ***This is critical to get UTC time in the files***
+6. Select the "Download by Year & Month" option, and choose the year and month you'd like to download. 
+7. Select "All Variables" for the "Available Variables" option. 
+8. Click "Retrieve Data" at the bottom. This will download a file called WBB.csv or similar for your station
+9. I like to rename this file to WBB.YYYYMM.csv, as otherwise they are not differentiated between different month
+   downloads. 
+10. Put the WBB.YYYYMM.csv file somewhere you know. 
+11. Navigate to the main function of this module, and alter the variables to match your system:
+    input_csv_fullpath = 'path/to/your/monthlydata/WBB.202311.csv' --name of the file you wish to split (what was downloaded above)
+    output_folder = 'path/to/your/output/folder/daily_csvs' -- path to a folder where the daily split files will be written (needs to exist)
+12. Run <python mesowest_met_handler.py> 
+13. There now should be daily files in the output folder that have form readable by GGG. Point to these files in GGG
+    or move them to the correct folder where GGG will find them. 
+'''
+
 import os
 import pandas as pd
 
@@ -11,19 +41,19 @@ def split_and_write_daily(input_csv_fullpath,header_change,output_folder):
     output_folder (str): path to a folder where the daily separated files will be stored
     '''
 
-    is_utc = utc_test(input_csv_fullpath) #test to make sure the data is in UTC
+    is_utc,site_id = utc_and_siteid(input_csv_fullpath) #test to make sure the data is in UTC
     if not is_utc:  #if not, raise an excepthion
         raise Exception('Monthly data file is not in UTC time, check your download settings and try again')
     print(f'Load the full dataframe for {input_csv_fullpath}')
     full_df = load_transform_fullmonth(input_csv_fullpath,header_change) #load the entire month into a dataframe
-    daily_data = split_into_daydfs(full_df) #split into days
+    daily_data = split_into_daydfs(full_df,site_id) #split into days
     for data in daily_data: #run through the days
         filename = data[0]
         df = data[1]
         print(f'Writing txt to {output_folder} for {filename}')
         write_to_txt(output_folder,filename,df) #and write them to txt files in the output folder
 
-def utc_test(input_csv_fullpath):
+def utc_and_siteid(input_csv_fullpath):
     '''Checks to make sure that the input mesowest file is in UTC time -- it should contain "UTC" in the data lines 
     instead of MDT or MST
     
@@ -37,10 +67,12 @@ def utc_test(input_csv_fullpath):
     with open(input_csv_fullpath) as f:
         for i in range(0,10):
             line = f.readline()
+            if line.startswith('# STATION:'):
+                site_id = line.split(' ')[-1]
         if 'UTC' not in line:
-            return False
+            return False,site_id
         else:
-            return True
+            return True,site_id
 
 def load_transform_fullmonth(input_csv_fullpath,header_change):
     '''Load and transform the full month of data, including the pressure value change and datetime parsing
@@ -65,7 +97,7 @@ def load_transform_fullmonth(input_csv_fullpath,header_change):
     full_df = full_df[keep_cols] #only keep the keep columns
     return full_df
         
-def split_into_daydfs(full_df):
+def split_into_daydfs(full_df,site_id):
     '''Splits a full month dataframe into individual days, and create the txt label based on date
     
     Args:
@@ -79,7 +111,7 @@ def split_into_daydfs(full_df):
     daily_data = [] #intitialize the list
     for group in full_df.groupby(full_df.index.date): #group by date
         day_df = group[1] #the df is the 1 element of the list
-        day_label = f'{group[0].year:04d}{group[0].month:02d}{group[0].day:02d}_WBB.txt' #this is the label of the file to be written
+        day_label = f'{group[0].year:04d}{group[0].month:02d}{group[0].day:02d}_{site_id}.txt' #this is the label of the file to be written
         daily_data.append([day_label,day_df]) #append the label and df to the list 
     return daily_data
 
