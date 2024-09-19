69b4561e9f4bb626ea9dff44c9007403d7fc843b
Aaron Meyer <agmeyer4@gmail.com>
2024-09-19 17:21:49 -0600

    started

---
+++ atmos_column/funcs/met_utils.py
@@ -0,0 +1,19 @@
+
+
+class MetColumnConfig:
+    
+    def __init__(self):
+        self.met_data = None
+        self.met_data_col_names = None
+        self.met_data_col_units = None
+        self.met_data_col_types
+
+class MetDataHandler:
+
+    default_col_names = {
+
+    }
+
+    def __init__(self):
+        pass
+
