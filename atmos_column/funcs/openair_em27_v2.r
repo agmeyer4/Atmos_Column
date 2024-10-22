2c0d50544c220d9338ef9c56d528ec20e4c96e2a
Aaron Meyer <agmeyer4@gmail.com>
2024-10-22 09:45:50 -0600

    openari

---
+++ atmos_column/funcs/openair_em27_v2.r
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(grid)
+library(openair)
+library(latticeExtra)
+
+
+ch4_co2_ha<-read.csv('/uufs/chpc.utah.edu/common/home/u0890904/LAIR_1/Projects/LAIR_mtg_2024/Data/202205-202409_5T_ch4_co2.csv')
+polarPlot(ch4_co2_ha,pollutant = 'ch4_co2_r2',#ws='ws_roll',wd='wd_roll',
+          statistic = 'percentile',percentile = 95,cols = 'viridis',par.settings=list(fontsize=list(text=25)))
+polarPlot(ch4_co2_ha,pollutant = 'ch4_co2_slope',#ws='ws_roll',wd='wd_roll',
+          statistic = 'percentile',percentile = 95,cols = 'viridis',par.settings=list(fontsize=list(text=25)))
+polarPlot(ch4_co2_ha,pollutant = 'ch4_co2_slope',#ws='ws_roll',wd='wd_roll',
+          statistic = 'mean',cols = 'viridis',par.settings=list(fontsize=list(text=25)))
+
+co_co2_ha<-read.csv('/uufs/chpc.utah.edu/common/home/u0890904/LAIR_1/Projects/LAIR_mtg_2024/Data/202205-202409_5T_co_co2.csv')
+polarPlot(co_co2_ha,pollutant = 'co_co2_slope',#ws='ws_roll',wd='wd_roll',
+          statistic = 'mean',cols = 'viridis',par.settings=list(fontsize=list(text=25)))
+polarPlot(co_co2_ha,pollutant = 'co_co2_slope',#ws='ws_roll',wd='wd_roll',
+          statistic = 'percentile',percentile = 95,cols = 'viridis',par.settings=list(fontsize=list(text=25)))
+polarPlot(co_co2_ha,pollutant = 'co_co2_r2',#ws='ws_roll',wd='wd_roll',
+          statistic = 'percentile',percentile = 95,cols = 'viridis',par.settings=list(fontsize=list(text=25)))
+
